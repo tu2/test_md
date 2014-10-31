5989697e6fc7d4b36552f62e115be9af4864ebae
tu2 <it@highstreetdesign.co.uk>
2014-10-31 15:55:38 +0000

    index table
    create tabel

--- index.php
+++ index.php
@@ -18,7 +18,22 @@ echo "</p>";
 
 echo "<h2>Index table</h2>\n";
 
+$raw_color = array('#F7D0C3', '#C3EAF7');
+$color_index = 0;
+$string = array(
+	'key1' => 'value1',
+	'key2' => 'value2',
+	'key3' => 'value3'
+	);
 
+	print "<table>\n";
+	foreach ($string as $k => $v){
+		print '<tr bgcolor="' . $raw_color[$color_index] . '" >';
+		print "<td> $k </td><td> $v </td>\n</tr>\n";
+		$color_index = 1 - $color_index;
+	}
+	print '</table>';
+	
 ?>
 
 
